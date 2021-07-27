13beb33d3ab54da43da75a644902f262392bc4ca
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-07-27 20:22:36 +0800

    fixed error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
     origin: ["http://localhost:8080"],
   })
 );
-const PORT = env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server started on port ");
 });
